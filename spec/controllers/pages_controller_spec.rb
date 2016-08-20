44d71ded0ba50affcd69e61a6a3343b8abd45f72
pejeguy <pejeguy@gmail.com>
2016-08-20 17:39:28 +0200

    Fini avec les pages statiques, chap.3

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,19 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe PagesController, type: :controller do
-
+  render_views
+  
   describe "GET #home" do
     it "returns http success" do
       get :home
       expect(response).to have_http_status(:success)
     end
+	
+
+    it "devrait avoir le bon titre" do
+      get :home
+      expect(response.body).to have_title('Simple App du Tutoriel Ruby on Rails | Accueil')
+    end	
   end
 
   describe "GET #contact" do
     it "returns http success" do
       get :contact
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(:success)    
+	end
+	
+	it "devrait avoir le bon titre" do
+      get :contact
+     expect(response.body).to have_title('Simple App du Tutoriel Ruby on Rails | Contact')
     end
   end
-
+  
+    describe "GET #about" do
+    it "returns http success" do
+      get :about
+      expect(response).to have_http_status(:success)
+    end
+	
+	it "devrait avoir le bon titre" do
+      get :about
+      expect(response.body).to have_title('Simple App du Tutoriel Ruby on Rails | À Propos')
+    end
+  end
 end
