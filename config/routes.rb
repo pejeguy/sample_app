1389b19a2f9e4a204ddfbf513db697e98b00e95e
pejeguy <pejeguy@gmail.com>
2016-08-21 20:37:37 +0200

    Fin du sign in out, chap.9

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   root 'pages#home'
   
-  get '/contact',   to: 'pages#contact'
+  get '/contact', to: 'pages#contact'
   get '/about',   to: 'pages#about'
-  get '/help',   to: 'pages#help'
-  get '/signup', to: 'users#new'
+  get '/help',    to: 'pages#help'
+  get '/signup',  to: 'users#new'
+  get '/signin',  to: 'sessions#new'
+  get '/signout', to: 'sessions#destroy'
+  
   resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
